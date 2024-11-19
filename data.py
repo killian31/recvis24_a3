a5afedd82a6340d4b23340dbfcb81e3f2979a86e
killian31 <80256683+killian31@users.noreply.github.com>
2024-11-19 10:57:24 +0100

    fix randomerasing tensor

--- data.py
+++ data.py
@@ -11,10 +11,10 @@ data_transforms = {
                 brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1
             ),
             transforms.RandomPerspective(distortion_scale=0.2, p=0.3),
+            transforms.ToTensor(),
             transforms.RandomErasing(
                 p=0.5, scale=(0.02, 0.15), ratio=(0.3, 3.3), value="random"
             ),
-            transforms.ToTensor(),
             transforms.Normalize(
                 mean=[0.853660523891449, 0.8536604642868042, 0.8536606431007385],
                 std=[0.2529744505882263, 0.25297433137893677, 0.2529739737510681],
